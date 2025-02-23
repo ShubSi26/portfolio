1cf6de77693ef344b78e44f11222d8e21790fb22
shubhamgit <shubhamsi160@gmail.com>
2025-02-24 01:26:35 +0530

    bug

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,7 @@ const nextConfig = {
         headers: [
           {
             key: "Content-Security-Policy",
+            value: "default-src 'self'; img-src 'self' https://skillicons.dev data:;",
             value: `
               default-src 'self';
               script-src 'self' 'unsafe-inline' 'unsafe-eval' https://challenges.cloudflare.com;
